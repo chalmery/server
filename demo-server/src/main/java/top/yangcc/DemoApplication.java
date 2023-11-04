d7a0dd5d58428b75062350ff44afc66e1c6ce228
四分之一个全栈 <95135709+chalmery@users.noreply.github.com>
2023-11-04 23:40:56 +0800

    demo

---
+++ demo-server/src/main/java/top/yangcc/DemoApplication.java
@@ -0,0 +1,17 @@
+package top.yangcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: ycc
+ * @date: 2023/7/23 上午9:45
+ */
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+}
